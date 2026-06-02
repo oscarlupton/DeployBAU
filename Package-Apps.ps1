d6730ffcdd06139aca118ddc6677448801182c84
Oscar Lupton <oscar.lupton@ticketek.com.au>
2026-06-02 15:32:29 +1000

    Auto-packaging

---
+++ Package-Apps.ps1
@@ -0,0 +1,14 @@
+$appList = 'Aspect', 'CRT', 'DCTops', 'Linkly'
+Set-Alias Start-7Zip ".\7z\7zr.exe"
+
+foreach ($app in $appList) {
+    $appDirectory = ".\$app\*"
+    $appConfig = ".\$app\config.txt"
+    $appArchive = ".\$app\$app.7z"
+    $appExecutable = ".\$app\$app.exe"
+
+    $7ZipParams = $('a', $appArchive, $appDirectory, "-x!$appConfig", "-x!$appArchive", "-x!$appExecutable")
+
+    Start-7Zip $7ZipParams
+    cmd /c "copy /b .\7z\7zSD.sfx + .\$app\config.txt + .\$app\$app.7z .\$app\$app.exe"
+}
